996063fe29d77844cf1591169d29125b04cdbad2
Bartuchy <bartekmark33@interia.pl>
2022-06-26 22:49:06 +0200

    initial commit

---
+++ swtod/src/main/java/com/example/swtod/SwtodApplication.java
@@ -0,0 +1,13 @@
+package com.example.swtod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwtodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwtodApplication.class, args);
+	}
+
+}
